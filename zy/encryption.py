fc11bacfdab8c609a7e7c9bb178d1afdd715d3af
Yang <evaniswaiting@hotmail.com>
2017-10-27 20:36:03 +0100

    Yang updates 20:35

--- zy/encryption.py
+++ zy/encryption.py
@@ -1,13 +1,17 @@
 import numpy as np
 import time
 import s_box
+import random
+
+KEYS = []
 
 def split_bits(mesg):
+    print mesg
     mesg_bits = []
-    row_bits = []
-    for i in range(mesg):
-        for j in range(mesg[i]):
-            row_bits.append(mesg[i][j])
+    for i in range(len(mesg)):
+        row_bits = []
+        for j in range(len(mesg[0][0])):
+            row_bits.append(mesg[i][0][j])
         mesg_bits.append(row_bits)
     return mesg_bits
 
@@ -25,16 +29,35 @@ def Permutation(mesg):
     mesg_bits = split_bits(mesg)
     row = len(mesg_bits)
     col = len(mesg_bits[0])
+    print 'bits:'
     print mesg_bits
-    for i in range(1, col)):
-        for j in range(row):
-            temp = mesg_bits[(j+i) % 8]
-            mesg_bits[(j+i % 8)] = 
+    mesg_bits = np.asarray(mesg_bits)
+    for i in range(0, col):
+	mesg_bits[:, i] = np.roll(mesg_bits[:,i], i)
+    return mesg_bits
+
+def Genkeys():
+    global KEYS
+    seed = random.randint(0, 30000)
+    key = ['{0:016b}'.format(seed)]
+    KEYS.append(key)   
+ 
+def BitewiseXOR(mesg):
+    keys = split_bits(KEYS)
+    Keyarray = np.asarray(keys)
+    #print 'Key'
+    #print int(Keyarray[0])
+    #print 'flatten'
+    #print int(mesg.flatten())
+    result = np.add(Keyarray[0].astype(int), mesg.flatten().astype(int)) % 2
+    print result.astype(str)
+    return result
             
-            
-        
 
 
 mesg = [['10111111'], ['01010101']]
 mesg = Subsititution(mesg)
-print mesg
+mesg_bits = Permutation(mesg)
+Genkeys()
+result = BitewiseXOR(mesg_bits)
+
